8fc575e2c355ebeca334bf885f9275ce61b05950
Enrico Nasca <enrico.nasca@uk-essen.de>
2022-06-20 15:14:19 +0000

    Grab log messages from cron.

--- cron/scripts/entrypoint.sh
+++ cron/scripts/entrypoint.sh
@@ -2,12 +2,15 @@
 
 DEBIAN_FRONTEND=noninteractive \
 	apt-get update \
-	&& apt-get -y install --no-install-recommends cron
+	&& apt-get -y install --no-install-recommends cron rsyslog
 
 userdel containeruser 2> /dev/null
 groupdel containeruser 2> /dev/null
 groupadd --gid "$USER_GID" containeruser \
 	&& useradd --no-log-init --uid "$USER_UID" --gid containeruser containeruser
+
+# Start syslogd. Log messages from cron will appear at /var/log/syslog.
+service rsyslog start
 
 # Start cron.
 exec /usr/sbin/cron -f
